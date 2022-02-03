0d3fcd564dad3d7d36f83692ab2c2d9ac977e4a2
Clement Loy <Clementloyme@gmail.com>
2022-02-03 16:44:39 +0800

    #6
    Completed to lesson #6
    3rd Feb 2022

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel nostrum numquam eligendi incidunt omnis reprehenderit, pariatur laudantium ipsum dicta minima consequatur illo inventore impedit dolore dolor explicabo ex deserunt qui!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel nostrum numquam eligendi incidunt omnis reprehenderit, pariatur laudantium ipsum dicta minima consequatur illo inventore impedit dolore dolor explicabo ex deserunt qui!</p>
+    </div>    
+    );
+}
+
+export default About;
